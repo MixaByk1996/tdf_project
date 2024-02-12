c97d9e6c352f2a365d168e536f45ba791f53f96f
MixaByk1996 <bykmikhail@gmail.com>
2024-02-12 09:27:50 +0300

    changes

--- resources/views/catalog.blade.php
+++ resources/views/catalog.blade.php
@@ -11,12 +11,12 @@
                     <span class="catalog-main-grides-text"><a href="/">Главная</a>/<a href="{{route('with-system', ['id' => 1])}}">Каталог</a></span>
                     <div class="show-products-container">
                     <div class="show-products-items">
-{{--                            <svg  id="svgOne" xmlns="http://www.w3.org/2000/svg" width="16" height="14" viewBox="0 0 16 14"--}}
-{{--                            fill="#EA600A">--}}
-{{--                                <path--}}
-{{--                                    d="M14 0H2C0.875 0 0 0.90625 0 2V12C0 13.125 0.875 14 2 14H14C15.0938 14 16 13.125 16 12V2C16 0.90625 15.0938 0 14 0ZM2 2H7V6H2V2ZM2 12V8H7V12H2ZM14 12H9V8H14V12ZM14 6H9V2H14V6Z"--}}
-{{--                                     />--}}
-{{--                            </svg>--}}
+                            <svg  id="svgOne" xmlns="http://www.w3.org/2000/svg" width="16" height="14" viewBox="0 0 16 14"
+                            fill="#EA600A">
+                                <path
+                                    d="M14 0H2C0.875 0 0 0.90625 0 2V12C0 13.125 0.875 14 2 14H14C15.0938 14 16 13.125 16 12V2C16 0.90625 15.0938 0 14 0ZM2 2H7V6H2V2ZM2 12V8H7V12H2ZM14 12H9V8H14V12ZM14 6H9V2H14V6Z"
+                                     />
+                            </svg>
                             <svg  id="svgTwo" xmlns="http://www.w3.org/2000/svg" width="16" height="14" viewBox="0 0 16 14"
                             fill="white"
                             >
@@ -229,11 +229,12 @@
                                 </svg>
                             </div>
                             <div class="filters-container-typechar-values">
-                                @foreach($series as $serie) @endforeach
+                                @foreach($series as $serie)
                                 <div class="filters-container-typechar-list-item">
                                     <input class="filters-container-typechar-chekbox" {{isset($_GET["serie_$serie->id"]) ? 'checked' : ''}} name="serie_{{$serie->id}}" type="checkbox" />
                                     <label class="filter-name-type">{{$serie->name}}</label>
                                 </div>
+                                @endforeach
 
                             </div>
                         </div>
