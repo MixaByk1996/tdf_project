5280ccc4db467e522ebb94f55f53ea89611282d6
Oleksandr <oleksandrzelenjak@gmail.com>
2024-03-08 14:37:05 +0200

    fixed bugs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
+
+    <meta name="format-detection" content="telephone=no">
+
+    
     <script src="/js/jquery.js"></script>
     <script src="/js/marquee.js"></script>
     <script type="text/javascript" src="/js/validate.js"></script>
